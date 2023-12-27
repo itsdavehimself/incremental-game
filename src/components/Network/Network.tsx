fa3d77e3afb46b4af3afa57ac7e1b9738efbb7a5
David <23064280+itsdavehimself@users.noreply.github.com>
2023-12-27 10:42:53 -0600

    Change display information

--- src/components/Network/Network.tsx
+++ src/components/Network/Network.tsx
@@ -1,10 +1,11 @@
 interface NetworkProps {
   gameState: {
     networks: number;
+    networksAvailable: number;
     GPUFarms: number;
     storageFacilities: number;
-    currentNodes: number;
-    totalNodes: number;
+    nodesCurrent: number;
+    nodesTotal: number;
     cognitum: number;
   };
 }
@@ -12,11 +13,14 @@ interface NetworkProps {
 const Network: React.FC<NetworkProps> = ({ gameState }) => {
   return (
     <div>
-      <div>Devices on Network: {gameState.networks}</div>
+      <div>Networks: {gameState.networks}</div>
+      <div>
+        Networks Available for Allocation: {gameState.networksAvailable}
+      </div>
       <div>Allocated as GPU Farms: {gameState.GPUFarms}</div>
       <div>Allocated as Storage Facilities: {gameState.storageFacilities}</div>
       <div>
-        Active nodes: {gameState.currentNodes}/{gameState.totalNodes}
+        Active nodes: {gameState.nodesCurrent}/{gameState.nodesTotal}
       </div>
       <div>Cognitum Mined: {gameState.cognitum}</div>
     </div>
