e373d4fd5d52af35241ce7a62c07736a381b1cc7
David <23064280+itsdavehimself@users.noreply.github.com>
2024-01-09 09:08:04 -0600

    Change data and time formatting

--- src/helpers/formatHelpers.ts
+++ src/helpers/formatHelpers.ts
@@ -1,30 +1,30 @@
 const formatData = (bytes: number): string => {
   if (bytes < 1024) {
-    return bytes.toFixed(0) + 'B';
+    return bytes.toFixed(0) + ' B';
   } else if (bytes < 1024 * 1024) {
-    return (bytes / 1024).toFixed(2) + 'KiB';
+    return (bytes / 1024).toFixed(2) + ' KiB';
   } else if (bytes < 1024 * 1024 * 1024) {
-    return (bytes / (1024 * 1024)).toFixed(2) + 'MiB';
+    return (bytes / (1024 * 1024)).toFixed(2) + ' MiB';
   } else if (bytes < 1024 * 1024 * 1024 * 1024) {
-    return (bytes / (1024 * 1024 * 1024)).toFixed(2) + 'GiB';
+    return (bytes / (1024 * 1024 * 1024)).toFixed(2) + ' GiB';
   } else if (bytes < 1024 * 1024 * 1024 * 1024 * 1024) {
-    return (bytes / (1024 * 1024 * 1024 * 1024)).toFixed(2) + 'TiB';
+    return (bytes / (1024 * 1024 * 1024 * 1024)).toFixed(2) + ' TiB';
   } else if (bytes < 1024 * 1024 * 1024 * 1024 * 1024 * 1024) {
-    return (bytes / (1024 * 1024 * 1024 * 1024 * 1024)).toFixed(2) + 'PiB';
+    return (bytes / (1024 * 1024 * 1024 * 1024 * 1024)).toFixed(2) + ' PiB';
   } else if (bytes < 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024) {
     return (
-      (bytes / (1024 * 1024 * 1024 * 1024 * 1024 * 1024)).toFixed(2) + 'EiB'
+      (bytes / (1024 * 1024 * 1024 * 1024 * 1024 * 1024)).toFixed(2) + ' EiB'
     );
   } else if (bytes < 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024) {
     return (
       (bytes / (1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024)).toFixed(2) +
-      'ZiB'
+      ' ZiB'
     );
   } else {
     return (
       (bytes / (1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024)).toFixed(
         2,
-      ) + 'YiB'
+      ) + ' YiB'
     );
   }
 };
@@ -34,9 +34,16 @@ const formatTimeElapsed = (totalSeconds: number) => {
   const minutes = Math.floor((totalSeconds % 3600) / 60);
   const seconds = Math.floor(totalSeconds % 60);
 
-  const formattedTime = `${hours}:${minutes < 10 ? '0' : ''}${minutes}:${
-    seconds < 10 ? '0' : ''
-  }${seconds}`;
+  const milliseconds = Math.floor(totalSeconds * 1000) % 1000;
+  const formattedMilliseconds = milliseconds
+    .toString()
+    .padStart(3, '0')
+    .slice(0, -1);
+
+  const formattedTime = `${hours < 10 ? '00' : ''}:${
+    minutes < 10 ? '0' : ''
+  }${minutes}:${seconds < 10 ? '0' : ''}${seconds}.${formattedMilliseconds}`;
+
   return formattedTime;
 };
 
