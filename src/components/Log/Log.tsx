3fb6bd8bdb0604de570ca9654ea5b8e20ff92244
David <23064280+itsdavehimself@users.noreply.github.com>
2024-01-08 18:37:22 -0600

    Scroll to bottom when new log messages appear

--- src/components/Log/Log.tsx
+++ src/components/Log/Log.tsx
@@ -1,13 +1,23 @@
 import { GameState } from '../../App';
 import styles from './Log.module.scss';
+import { useEffect, useRef } from 'react';
 
 interface LogProps {
   gameState: GameState;
 }
 
 const Log: React.FC<LogProps> = ({ gameState }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const scrollToBottom = () => {
+    if (containerRef.current) {
+      containerRef.current.scrollTop = containerRef.current.scrollHeight;
+    }
+  };
+  useEffect(() => {
+    scrollToBottom();
+  }, [gameState.logMessages]);
   return (
-    <div className={styles['log-container']}>
+    <div ref={containerRef} className={styles['log-container']}>
       {gameState.logMessages.map((message) => (
         <p>{message}</p>
       ))}
