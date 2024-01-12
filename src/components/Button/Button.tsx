e1b931269a20c1e645f7d116acab631bb127efbd
David <23064280+itsdavehimself@users.noreply.github.com>
2024-01-12 13:45:26 -0600

    Create main button styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,19 +2,34 @@ import styles from './Button.module.scss';
 
 interface ButtonProps {
   onClick: () => void;
-  label: string;
+  upgradeName: string;
+  upgradeCost: string;
   disabled: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ onClick, label, disabled }) => {
+const Button: React.FC<ButtonProps> = ({
+  onClick,
+  upgradeName,
+  upgradeCost,
+  disabled,
+}) => {
   return (
-    <button
-      className={styles['main-btn']}
-      onClick={onClick}
-      disabled={disabled}
-    >
-      {label}
-    </button>
+    <>
+      <button
+        className={styles['main-btn']}
+        onClick={onClick}
+        disabled={disabled}
+      >
+        <div className={styles['btn-text']}>
+          <h4>{upgradeName}</h4>
+          <p>{upgradeCost}</p>
+        </div>
+      </button>
+      <div className={styles['line-container']}>
+        <div className={styles.line}></div>
+        <div className={styles.rectangle}></div>
+      </div>
+    </>
   );
 };
 
