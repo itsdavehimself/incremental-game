2c4a9f605c9396f9d170d5ab3907084773e01119
David <23064280+itsdavehimself@users.noreply.github.com>
2024-01-15 14:10:39 -0600

    Create footer navigation icons

--- src/components/FooterNav/FooterNav.tsx
+++ src/components/FooterNav/FooterNav.tsx
@@ -1,13 +1,61 @@
+import NetworkIcon from '../icons/NetworkIcon';
+import ProcessorIcon from '../icons/ProcessorIcon';
+import ExplorerIcon from '../icons/ExplorerIcon';
 import styles from './FooterNav.module.scss';
+import UpgradeIcon from '../icons/UpgradeIcon';
+import DecryptIcon from '../icons/DecryptIcon';
+import { GameState } from '../../App';
 
-const FooterNav: React.FC = () => {
+interface FooterNavProps {
+  currentView: string;
+  setCurrentView: React.Dispatch<
+    React.SetStateAction<
+      'home' | 'files' | 'upgrades' | 'networks' | 'upgrades' | 'wallet'
+    >
+  >;
+  gameState: GameState;
+}
+
+const FooterNav: React.FC<FooterNavProps> = ({ setCurrentView, gameState }) => {
   return (
     <nav className={styles.footer}>
-      <button>HOME</button>
-      <button>FILES</button>
-      <button>NETWORKS</button>
-      <button>UPGRADES</button>
-      <button>DECRYPTION</button>
+      <button onClick={() => setCurrentView('home')}>
+        <div className={styles['nav-icon']}>
+          <ProcessorIcon />
+        </div>
+      </button>
+      <button
+        onClick={() => setCurrentView('files')}
+        disabled={!gameState.filesActivated}
+      >
+        <div className={styles['nav-icon']}>
+          <ExplorerIcon />
+        </div>
+      </button>
+      <button
+        onClick={() => setCurrentView('networks')}
+        disabled={!gameState.networksActivated}
+      >
+        <div className={styles['nav-icon']}>
+          <NetworkIcon />
+        </div>
+      </button>
+      <button
+        onClick={() => setCurrentView('upgrades')}
+        disabled={!gameState.networksActivated}
+      >
+        <div className={styles['nav-icon']}>
+          <UpgradeIcon />
+        </div>
+      </button>
+      <button
+        onClick={() => setCurrentView('wallet')}
+        disabled={!gameState.walletDecryptionActivated}
+      >
+        <div className={styles['nav-icon']}>
+          <DecryptIcon />
+        </div>
+      </button>
     </nav>
   );
 };
---
+++ src/components/icons/Arrow.tsx
@@ -0,0 +1,15 @@
+export default function Arrow() {
+  return (
+    <svg
+      fill="#dedede"
+      width="1.4rem"
+      height="1.4rem"
+      data-name="Layer 1"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 393.77 391.74"
+    >
+      <defs></defs>
+      <polygon points="348.64 346.61 83.09 346.61 393.2 36.5 353.78 0 45.13 308.65 45.13 45.7 0 .57 0 391.74 393.77 391.74 348.64 346.61" />
+    </svg>
+  );
+}
