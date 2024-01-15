f2b3c6628ba9c531250948e88e6fc6e0c6db6342
David <23064280+itsdavehimself@users.noreply.github.com>
2024-01-15 14:11:15 -0600

    Remove i in byte formats

--- src/helpers/formatHelpers.ts
+++ src/helpers/formatHelpers.ts
@@ -2,29 +2,29 @@ const formatData = (bytes: number): string => {
   if (bytes < 1024) {
     return bytes.toFixed(0) + ' B';
   } else if (bytes < 1024 * 1024) {
-    return (bytes / 1024).toFixed(2) + ' KiB';
+    return (bytes / 1024).toFixed(2) + ' KB';
   } else if (bytes < 1024 * 1024 * 1024) {
-    return (bytes / (1024 * 1024)).toFixed(2) + ' MiB';
+    return (bytes / (1024 * 1024)).toFixed(2) + ' MB';
   } else if (bytes < 1024 * 1024 * 1024 * 1024) {
-    return (bytes / (1024 * 1024 * 1024)).toFixed(2) + ' GiB';
+    return (bytes / (1024 * 1024 * 1024)).toFixed(2) + ' GB';
   } else if (bytes < 1024 * 1024 * 1024 * 1024 * 1024) {
-    return (bytes / (1024 * 1024 * 1024 * 1024)).toFixed(2) + ' TiB';
+    return (bytes / (1024 * 1024 * 1024 * 1024)).toFixed(2) + ' TB';
   } else if (bytes < 1024 * 1024 * 1024 * 1024 * 1024 * 1024) {
-    return (bytes / (1024 * 1024 * 1024 * 1024 * 1024)).toFixed(2) + ' PiB';
+    return (bytes / (1024 * 1024 * 1024 * 1024 * 1024)).toFixed(2) + ' PB';
   } else if (bytes < 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024) {
     return (
-      (bytes / (1024 * 1024 * 1024 * 1024 * 1024 * 1024)).toFixed(2) + ' EiB'
+      (bytes / (1024 * 1024 * 1024 * 1024 * 1024 * 1024)).toFixed(2) + ' EB'
     );
   } else if (bytes < 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024) {
     return (
       (bytes / (1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024)).toFixed(2) +
-      ' ZiB'
+      ' ZB'
     );
   } else {
     return (
       (bytes / (1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024)).toFixed(
         2,
-      ) + ' YiB'
+      ) + ' YB'
     );
   }
 };
