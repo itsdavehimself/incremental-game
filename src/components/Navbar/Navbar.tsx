9aedd3c9701c778b0a658905a3bab0499ee1e9d9
David <23064280+itsdavehimself@users.noreply.github.com>
2024-01-15 14:12:06 -0600

    Add Navbar styles and render logic

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,30 @@
 import styles from './Navbar.module.scss';
 import { GameState } from '../../App';
 import { formatData } from '../../helpers/formatHelpers';
+import SaveGameIcon from '../icons/SaveGameIcon';
 
 interface NavbarProps {
   gameState: GameState;
+  currentView:
+    | 'home'
+    | 'files'
+    | 'upgrades'
+    | 'networks'
+    | 'upgrades'
+    | 'wallet';
 }
 
-const Navbar: React.FC<NavbarProps> = ({ gameState }) => {
+const Navbar: React.FC<NavbarProps> = ({ gameState, currentView }) => {
   return (
     <nav className={styles.navbar}>
       <div className={styles.logo}>LOGO</div>
-      <div className={styles.data}>{formatData(gameState.totalData)}</div>
-      <div className={styles.hamburger}>SAVE</div>
+      {currentView !== 'home' && (
+        <div className={styles.data}>{formatData(gameState.totalData)}</div>
+      )}
+
+      <div className={styles.save}>
+        <SaveGameIcon />
+      </div>
     </nav>
   );
 };
