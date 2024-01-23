4624d760c3860223c6826f2abb177e673a2298e6
David <23064280+itsdavehimself@users.noreply.github.com>
2024-01-23 10:36:20 -0600

    Create formatHelpers tests

---
+++ src/helpers/tests/formatHelpers.test.ts
@@ -0,0 +1,47 @@
+import { formatData } from '../formatHelpers';
+import { formatTimeElapsed } from '../formatHelpers';
+import { describe, expect, it } from 'vitest';
+
+describe('#formatData', () => {
+  it('returns string: 100 B', () => {
+    expect(formatData(100)).toBe('100 B');
+  });
+
+  it('returns string: 1 KB', () => {
+    expect(formatData(1024)).toBe('1.00 KB');
+  });
+
+  it('returns string: 2.46 MB', () => {
+    expect(formatData(2580000)).toBe('2.46 MB');
+  });
+
+  it('returns string: 2.46 MB', () => {
+    expect(formatData(2580000)).toBe('2.46 MB');
+  });
+
+  it('returns string: 5.00 GB', () => {
+    expect(formatData(5370000000)).toBe('5.00 GB');
+  });
+
+  it('returns string: 1.00 TB', () => {
+    expect(formatData(1100000000000)).toBe('1.00 TB');
+  });
+});
+
+describe('#formatTimeElapsed', () => {
+  it('returns seconds in format: 00:00:01.00 ', () => {
+    expect(formatTimeElapsed(1)).toBe('00:00:01.00');
+  });
+
+  it('returns seconds in format: 00:01:00.00 ', () => {
+    expect(formatTimeElapsed(60)).toBe('00:01:00.00');
+  });
+
+  it('returns seconds in format: 00:10:00.00 ', () => {
+    expect(formatTimeElapsed(600)).toBe('00:10:00.00');
+  });
+
+  it('returns seconds in duration format: 00:20:00.00 ', () => {
+    expect(formatTimeElapsed(1200)).toBe('00:20:00.00');
+  });
+});
